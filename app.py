61857f1249eb8a010907ea49b5d01bd4166cb04c
sizdah <sina7th@gmail.com>
2019-03-06 12:32:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,178 +1,70 @@
 import logging
-import requests ,re
-from bs4 import BeautifulSoup
 from queue import Queue
 from threading import Thread
 from telegram import Bot,ReplyKeyboardMarkup,ReplyKeyboardRemove
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters
+import re,glob,os
+
+import feedparser
+from googletrans import Translator
 
 
-
+lock = False
+query_mem = ""
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = '565668483:AAEgjE0vELcfjk2L05SGLtj0Hc5-XY9unyo' 
+TOKEN = '691443857:AAHX-40RGGdBG-2d_aAZyFKdSbQ0VcYvTHc'
+
+
+
+
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+
+def get(bot, update):
+    menza = "https://jidelnicek.utb.cz/WebKredit/rss?canteenId=3&language=En"
+
+    d = feedparser.parse(menza)
+    data = cleanhtml(d.entries[1]["summary"])
+
+
+    translator = Translator()
+
+    en_data = translator.translate(data, dest='en')
+
+    update.message.reply_text('Okay my foodie friend, I will send you U5 Menza foods NOW'+"\n"+data+"\n"+str(en_data))
 
 
 def start(bot, update):
-    update.message.reply_text('با زدن عبارت phd/ میتونید لیست بورسیه های دکترا رو ببینید')
-    update.message.reply_text('با زدن عبارت master/ میتونید لیست بورسیه های فوق لیسانس رو ببینید')
-    update.message.reply_text('با زدن عبارت undergraduate/ میتونید لیست بورسیه های لیسانس و قبل از اون رو ببینید')
-
-    bot = Bot(TOKEN)
-    id = update.message.from_user.id
-    id = int(id)
-
-    custom_keyboard = [
-        ['/PhD'],
-        ['/Master'],
-        ['/Undergraduate']
-    ]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id=id, text="انتخاب کنید", reply_markup=reply_markup)
-
-
-
-def phd(bot, update):
-
-    bot = Bot(TOKEN)
-    id = update.message.from_user.id
-    id = int(id)
-
-    reply_markup = ReplyKeyboardRemove()
-    bot.send_message(chat_id=id, text="آماده سازی", reply_markup=reply_markup)
-
-    try:
-
-        try:
-            r = requests.get("https://scholarship-positions.com/category/phd-scholarships-positions")
-        except:
-            print("can not connect , retry in 60 secs...")
-
-
-        c = r.content
-        soup = BeautifulSoup(c, "html.parser")
-        soupo = soup.find_all("h2", {"class": "title entry-title"})
-
-
-
-        for item in soupo:
-
-            mes = str(item.text)
-
-            link = item.find_next('a', attrs={'href': re.compile("^https://")})
-            print(link.get('href'))
-            
-            link2 = str(link.get('href'))
-
-
-
-            bot.send_message(chat_id=id, text=mes)
-            bot.send_message(chat_id=id, text=link2)
-            #update.message.reply_text(mes)
-            #update.message.reply_text(link)
-
-
-            # print(item.text)
-            # date= jdate.gregorian_to_jd()
-    except KeyboardInterrupt:
-        print("failed")
-
-
-def master(bot, update):
-
-
-    bot = Bot(TOKEN)
-    id = update.message.from_user.id
-    id = int(id)
-
-    reply_markup = ReplyKeyboardRemove()
-    bot.send_message(chat_id=id, text="آماده سازی", reply_markup=reply_markup)
-
-
-    try:
-
-        try:
-            r = requests.get("https://scholarship-positions.com/category/masters-scholarships/")
-        except:
-            print("can not connect , retry in 60 secs...")
-
-        c = r.content
-        soup = BeautifulSoup(c, "html.parser")
-        soupo = soup.find_all("h2", {"class": "title entry-title"})
-
-
-
-        for item in soupo:
-            mes = str(item.text)
-
-            link = item.find_next('a', attrs={'href': re.compile("^https://")})
-            print(link.get('href'))
-
-            link2 = str(link.get('href'))
-
-
-            bot.send_message(chat_id=id, text=mes)
-            bot.send_message(chat_id=id, text=link2)
-            # update.message.reply_text(mes)
-            # update.message.reply_text(link)
-
-
-            # print(item.text)
-            # date= jdate.gregorian_to_jd()
-    except KeyboardInterrupt:
-        print("failed")
-
-
-
-def undergraduate(bot, update):
-
-    bot = Bot(TOKEN)
-    id = update.message.from_user.id
-    id = int(id)
-
-    reply_markup = ReplyKeyboardRemove()
-    bot.send_message(chat_id=id, text="آماده سازی", reply_markup=reply_markup)
-
-
-    try:
-
-        try:
-            r = requests.get("https://scholarship-positions.com/category/under-graduate-scholarship/")
-        except:
-            print("can not connect , retry in 60 secs...")
-
-        c = r.content
-        soup = BeautifulSoup(c, "html.parser")
-        soupo = soup.find_all("h2", {"class": "title entry-title"})
-
-
-
-        for item in soupo:
-            mes = str(item.text)
-
-            link = item.find_next('a', attrs={'href': re.compile("^https://")})
-            print(link.get('href'))
-
-            link2 = str(link.get('href'))
-
-
-            bot.send_message(chat_id=id, text=mes)
-            bot.send_message(chat_id=id, text=link2)
-            # update.message.reply_text(mes)
-            # update.message.reply_text(link)
-
-
-            # print(item.text)
-            # date= jdate.gregorian_to_jd()
-    except KeyboardInterrupt:
-        print("failed")
+    update.message.reply_text('Okay my foodie friend, type /get to get the list')
 
 
 def echo(bot, update):
-    update.message.reply_text("از دستور راهنمایی استفاده کنید")
-    update.message.reply_text("/help")
+    update.message.reply_text('Okay my foodie friend, I will send you U5 Menza foods NOW')
+    menza = "https://jidelnicek.utb.cz/WebKredit/rss?canteenId=3&language=En"
+
+    d = feedparser.parse(menza)
+    data = cleanhtml(d.entries[1]["summary"])
+
+
+    translator = Translator()
+
+    en_data = translator.translate(data, dest='en')
+
+
+    update.message.reply_text(data+"\n"+en_data)
+
+
+
+
+
+
 
 
 
@@ -183,6 +75,8 @@ def error(bot, update, error):
 
 
 def setup(webhook_url=None):
+
+
     """If webhook_url is not passed, run with long-polling."""
     logging.basicConfig(level=logging.WARNING)
     if webhook_url:
@@ -195,9 +89,9 @@ def setup(webhook_url=None):
         dp = updater.dispatcher
         dp.add_handler(CommandHandler("start", start))
         dp.add_handler(CommandHandler("help", start))
-        dp.add_handler(CommandHandler("phd", phd))
-        dp.add_handler(CommandHandler("master", master))
-        dp.add_handler(CommandHandler("undergraduate", undergraduate))
+        dp.add_handler(CommandHandler("get", get))
+
+
         # on noncommand i.e message - echo the message on Telegram
         dp.add_handler(MessageHandler(Filters.text, echo))
 
